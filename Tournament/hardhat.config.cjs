7549626045993501655c3c85e6017044fe805bab
Rhnizar <ridarhnizar1337@gmail.com>
2024-09-15 19:43:48 +0100

    hardhat test it work now

--- Tournament/hardhat.config.cjs
+++ Tournament/hardhat.config.cjs
@@ -1,14 +1,6 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-ethers"); // Add this line to require the Ethers plugin
 
-// import "@nomiclabs/hardhat-ethers";
-
 module.exports = {
   solidity: "0.8.24",
-  // networks: {
-  //   sepolia: {
-  //     url: process.env.SEPOLIA_URL, // RPC URL from Infura, Alchemy, etc.
-  //     accounts: [`0x${process.env.PRIVATE_KEY}`] // Your wallet private key
-  //   }
-  // }
 };
